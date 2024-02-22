9463f8b17ed659ea5e09b6153a3f568acfcc3727
Cody Hoffman <choffman733@gmail.com>
2024-02-21 20:38:04 -0600

    feat: setup vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 
@@ -37,6 +38,7 @@ export default function RootLayout({
         >
           {children}
           <Toolbar />
+          <Analytics />
           <Toaster />
         </ThemeProvider>
       </body>
