c0b18be1e70acdb0fda9f7af150c66f792f6b7da
Cody Hoffman <choffman733@gmail.com>
2024-02-29 20:09:22 -0600

    feat: add hidden domains page

---
+++ src/app/domains/page.tsx
@@ -0,0 +1,16 @@
+export default function Domains() {
+  return (
+    <main className="flex flex-col p-8 pt-16  md:p-24 max-w-[600px] mx-auto mb-28">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight md:text-5xl">
+        Interested in a domain I own?
+      </h1>
+      <p className="mt-8 text-xl text-muted-foreground tracking-wide">
+        Click the message bubble in the toolbar to let me know the domain
+        you&apos;re interested in.
+      </p>
+      <p className="mt-6 text-xl text-muted-foreground tracking-wide">
+        I&apos;ll get back to you as soon as possible.
+      </p>
+    </main>
+  );
+}
